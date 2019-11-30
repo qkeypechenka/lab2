d02c7d5f8c078fbdf4e08dc294142fe150fe2214
qkey <petrovsergy@outlook.com>
2019-11-30 17:10:57 +0500

    update clearing

--- graph_maker.py
+++ graph_maker.py
@@ -1,15 +1,13 @@
 from matplotlib import pyplot as plt
-from collections import deque
 import serial
 
 ser = serial.Serial("/dev/cu.usbserial-1410", 115200)
 plt.ion()
 
-distances = deque(maxlen=20)
-
+distances = [0] * 20
 line, = plt.plot(distances)
 plt.ylim([10, 40])
-plt.axes(projection='polar')
+axes = plt.axes(projection='polar')
 plt.show()
 while True:
     while ser.in_waiting == 0:
@@ -20,10 +18,14 @@ while True:
     distances.append(distance)
     ymin = float(min(distances)) - 20
     ymax = float(max(distances)) + 20
-    plt.ylim([ymin, ymax])
+    plt.ylim([ymin, 100])
+    distances = distances[1:]
 
     line.set_xdata(len(distances))
     line.set_ydata(distances)
     plt.polar(angle, distance, 'p')
     plt.draw()
     plt.pause(1e-19)
+    line.set_ydata([])
+    if angle < 0.0001:
+        axes.clear()
